13d3805cf70ab2c06bf2fe6054b405c187174fdb
Javie <Javie@Javi-PC>
2025-11-26 21:40:37 -0300

    barra de menu con sus caracteristicas

--- src/vista/VentanaPrincipal.java
+++ src/vista/VentanaPrincipal.java
@@ -12,11 +12,18 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenuItem1 = new javax.swing.JMenuItem();
         jLabel1 = new javax.swing.JLabel();
-        btn_Ingresar = new javax.swing.JButton();
-        btn_Mostrar = new javax.swing.JButton();
-        btn_Salir = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jmi_ingresar = new javax.swing.JMenuItem();
+        jmi_mostrar = new javax.swing.JMenuItem();
+        jSeparator1 = new javax.swing.JPopupMenu.Separator();
+        jmi_salir = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+
+        jMenuItem1.setText("jMenuItem1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setAutoRequestFocus(false);
@@ -26,103 +33,107 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
         jLabel1.setText("Atención Podologica");
 
-        btn_Ingresar.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        btn_Ingresar.setText("Ingresar Hora");
-        btn_Ingresar.addActionListener(new java.awt.event.ActionListener() {
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/podosano.png"))); // NOI18N
+
+        jMenu1.setText("Menu");
+
+        jmi_ingresar.setText("Ingresar Hora");
+        jmi_ingresar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn_IngresarActionPerformed(evt);
+                jmi_ingresarActionPerformed(evt);
             }
         });
+        jMenu1.add(jmi_ingresar);
 
-        btn_Mostrar.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        btn_Mostrar.setText("Mostrar Horas");
-        btn_Mostrar.addActionListener(new java.awt.event.ActionListener() {
+        jmi_mostrar.setText("Mostrar Horas");
+        jmi_mostrar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn_MostrarActionPerformed(evt);
+                jmi_mostrarActionPerformed(evt);
             }
         });
+        jMenu1.add(jmi_mostrar);
+        jMenu1.add(jSeparator1);
 
-        btn_Salir.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        btn_Salir.setText("Salir");
-        btn_Salir.addActionListener(new java.awt.event.ActionListener() {
+        jmi_salir.setText("Salir");
+        jmi_salir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn_SalirActionPerformed(evt);
+                jmi_salirActionPerformed(evt);
             }
         });
+        jMenu1.add(jmi_salir);
 
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/PodoSano_1.png"))); // NOI18N
+        jMenuBar1.add(jMenu1);
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(161, 161, 161)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(81, Short.MAX_VALUE)
-                .addComponent(jLabel4)
-                .addGap(34, 34, 34)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(btn_Salir, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(btn_Ingresar, javax.swing.GroupLayout.DEFAULT_SIZE, 147, Short.MAX_VALUE)
-                    .addComponent(btn_Mostrar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(32, 32, 32))
+                .addGap(193, 193, 193))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(35, 35, 35)
+                .addComponent(jLabel3)
+                .addContainerGap(34, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(37, 37, 37)
+                .addGap(40, 40, 40)
                 .addComponent(jLabel1)
-                .addGap(20, 20, 20)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(btn_Ingresar, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(btn_Mostrar, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(60, 60, 60))
-                    .addComponent(jLabel4))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(btn_Salir, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(39, Short.MAX_VALUE))
+                .addGap(37, 37, 37)
+                .addComponent(jLabel3)
+                .addContainerGap(100, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btn_MostrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_MostrarActionPerformed
-        VentanaMostrar VentanaMostrar = new VentanaMostrar();
-        VentanaMostrar.setTitle("Horas");
-        VentanaMostrar.setLocationRelativeTo(null);
-        VentanaMostrar.setResizable(false); //modificar tamaño de la ventana
-        VentanaMostrar.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE); //No cerrar la ventana con la X
-        VentanaMostrar.setVisible(true);
-    }//GEN-LAST:event_btn_MostrarActionPerformed
-
-    private void btn_SalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_SalirActionPerformed
-        int opcion = JOptionPane.showConfirmDialog(this, "¿Seguro?");
-        
-        if (opcion == 0){
-            dispose();
-        }
-    }//GEN-LAST:event_btn_SalirActionPerformed
-
-    private void btn_IngresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_IngresarActionPerformed
+    private void jmi_ingresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_ingresarActionPerformed
         VentanaIngresar ventanaIngresar = new VentanaIngresar();
         ventanaIngresar.setTitle("Ingresar");
         ventanaIngresar.setLocationRelativeTo(null);
         ventanaIngresar.setResizable(false); //modificar tamaño de la ventana
         ventanaIngresar.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE); //No cerrar la ventana con la X
         ventanaIngresar.setVisible(true);
-    }//GEN-LAST:event_btn_IngresarActionPerformed
+    }//GEN-LAST:event_jmi_ingresarActionPerformed
+
+    private void jmi_mostrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_mostrarActionPerformed
+        VentanaMostrar VentanaMostrar = new VentanaMostrar();
+        VentanaMostrar.setTitle("Horas");
+        VentanaMostrar.setLocationRelativeTo(null);
+        VentanaMostrar.setResizable(false); //modificar tamaño de la ventana
+        VentanaMostrar.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE); //No cerrar la ventana con la X
+        VentanaMostrar.setVisible(true);
+    }//GEN-LAST:event_jmi_mostrarActionPerformed
+
+    private void jmi_salirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_salirActionPerformed
+        int resp = JOptionPane.showConfirmDialog(
+            null,
+            "¿Desea continuar?",
+            "Confirmación de salida",
+            JOptionPane.YES_NO_OPTION
+    );
+        if (resp == 0){
+            dispose();
+        }
+    }//GEN-LAST:event_jmi_salirActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btn_Ingresar;
-    private javax.swing.JButton btn_Mostrar;
-    private javax.swing.JButton btn_Salir;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JPopupMenu.Separator jSeparator1;
+    private javax.swing.JMenuItem jmi_ingresar;
+    private javax.swing.JMenuItem jmi_mostrar;
+    private javax.swing.JMenuItem jmi_salir;
     // End of variables declaration//GEN-END:variables
 }
